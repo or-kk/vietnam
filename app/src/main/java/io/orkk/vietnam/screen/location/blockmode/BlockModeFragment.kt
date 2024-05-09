1b9aace6cfbe5cae061c39925ebceb41809713ad
or-kk <tmdrl1026@naver.com>
2024-05-09 16:36:19 +0900

    add rotate screen orientation for block mode

--- app/src/main/java/io/orkk/vietnam/screen/location/blockmode/BlockModeFragment.kt
+++ app/src/main/java/io/orkk/vietnam/screen/location/blockmode/BlockModeFragment.kt
@@ -1,11 +1,38 @@
 package io.orkk.vietnam.screen.location.blockmode
 
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import dagger.hilt.android.AndroidEntryPoint
 import io.orkk.vietnam.R
 import io.orkk.vietnam.databinding.FragmentBlockModeBinding
 import io.orkk.vietnam.screen.BaseFragment
+import io.orkk.vietnam.utils.extension.launchAndRepeatWithViewLifecycle
 
 @AndroidEntryPoint
 class BlockModeFragment : BaseFragment<FragmentBlockModeBinding>(R.layout.fragment_block_mode) {
 
+    private val blockModeViewModel: BlockModeViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        dataBinding.apply {
+            lifecycleOwner = viewLifecycleOwner
+            viewModel = blockModeViewModel
+        }
+    }
+
+    override fun initObserver() {
+        super.initObserver()
+
+        launchAndRepeatWithViewLifecycle {
+            blockModeViewModel.currentScreenOrientation.observe(viewLifecycleOwner, Observer {
+                activity?.run {
+                    requestedOrientation = it
+                }
+            })
+        }
+    }
 }
--- app/src/main/java/io/orkk/vietnam/screen/location/blockmode/BlockModeViewModel.kt
+++ app/src/main/java/io/orkk/vietnam/screen/location/blockmode/BlockModeViewModel.kt
@@ -1,12 +1,25 @@
 package io.orkk.vietnam.screen.location.blockmode
 
+import android.content.pm.ActivityInfo
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.orkk.vietnam.screen.BaseViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class BlockModeViewModel @Inject constructor(
+class BlockModeViewModel @Inject constructor() : BaseViewModel() {
+    private val _currentScreenOrientation = MutableLiveData<Int>()
+    val currentScreenOrientation: LiveData<Int>
+        get() = _currentScreenOrientation
 
-) : BaseViewModel() {
+    init {
+        _currentScreenOrientation.value = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+    }
 
+    fun setScreenOrientation() {
+        val currentOrientation = _currentScreenOrientation.value ?: return
+        _currentScreenOrientation.value = if (currentOrientation == ActivityInfo.SCREEN_ORIENTATION_PORTRAIT) ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+        else ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+    }
 }
