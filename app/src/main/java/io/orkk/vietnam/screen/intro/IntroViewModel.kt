2e70b97783cf8437ef734e9f6c51dab163d85a86
or-kk <tmdrl1026@naver.com>
2024-05-31 16:53:24 +0900

    wip ftp module

--- app/src/main/java/io/orkk/vietnam/screen/intro/IntroViewModel.kt
+++ app/src/main/java/io/orkk/vietnam/screen/intro/IntroViewModel.kt
@@ -2,13 +2,18 @@ package io.orkk.vietnam.screen.intro
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import io.orkk.vietnam.data.remote.FileRepository
 import io.orkk.vietnam.screen.BaseViewModel
 import io.orkk.vietnam.utils.event.Event
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class IntroViewModel @Inject constructor() : BaseViewModel() {
+class IntroViewModel @Inject constructor(
+    private val fileRepository: FileRepository
+) : BaseViewModel() {
 
     private val _navigateToSignIn = MutableLiveData<Event<Unit>>()
     val navigateToSignIn: LiveData<Event<Unit>>
@@ -16,5 +21,18 @@ class IntroViewModel @Inject constructor() : BaseViewModel() {
 
     fun navigateToMain() {
         _navigateToSignIn.value = Event(Unit)
+    }
+
+    fun downloadAllAppData(remoteFilePath: String, localFilePath: String) {
+        viewModelScope.launch {
+            fileRepository.downloadFile(remoteFilePath = remoteFilePath, localFilePath = localFilePath).apply {
+                onSuccess {
+
+                }
+                onFailure {
+
+                }
+            }
+        }
     }
 }
