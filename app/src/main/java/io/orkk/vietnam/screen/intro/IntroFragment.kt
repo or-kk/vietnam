4b3d78fa837856bde131a650923c02fb5f0be1d6
or-kk <tmdrl1026@naver.com>
2024-06-11 16:52:41 +0900

    refactor intro

--- app/src/main/java/io/orkk/vietnam/screen/intro/IntroFragment.kt
+++ app/src/main/java/io/orkk/vietnam/screen/intro/IntroFragment.kt
@@ -4,15 +4,16 @@ package io.orkk.vietnam.screen.intro
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
 import io.orkk.vietnam.R
 import io.orkk.vietnam.databinding.FragmentIntroBinding
 import io.orkk.vietnam.screen.BaseFragment
 import io.orkk.vietnam.screen.intro.initial.InitialSettingViewModel
-import io.orkk.vietnam.utils.event.EventObserver
 import io.orkk.vietnam.utils.extension.launchAndRepeatWithViewLifecycle
 import io.orkk.vietnam.utils.extension.safeNavigate
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class IntroFragment : BaseFragment<FragmentIntroBinding>(R.layout.fragment_intro) {
@@ -22,7 +23,6 @@ class IntroFragment : BaseFragment<FragmentIntroBinding>(R.layout.fragment_intro
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         dataBinding.apply {
             lifecycleOwner = viewLifecycleOwner
             viewModel = introViewModel
@@ -32,29 +32,28 @@ class IntroFragment : BaseFragment<FragmentIntroBinding>(R.layout.fragment_intro
 
     override fun initView() {
         super.initView()
-    }
-
-    override fun initListener() {
-        super.initListener()
+        showLoading()
     }
 
     override fun initObserver() {
         super.initObserver()
 
         launchAndRepeatWithViewLifecycle {
-            initialSettingViewModel.isExistClubInfo.collect {
-                if (it) findNavController().safeNavigate(IntroFragmentDirections.actionIntroFragmentToSignInFragment())
-                else findNavController().safeNavigate(IntroFragmentDirections.actionIntroFragmentToInitialDialogFragment())
+            launch {
+                introViewModel.loading.observe(viewLifecycleOwner, Observer {
+                    if (it) showLoading() else hideLoading()
+                })
             }
-        }
 
-        introViewModel.navigateToSignIn.observe(viewLifecycleOwner, EventObserver {
-            activity?.run {
-                if (isTaskRoot) {
-//                    findNavController().safeNavigate(IntroFragmentDirections.actionIntroFragmentToSignInFragment())
-                    findNavController().safeNavigate(IntroFragmentDirections.actionIntroFragmentToDownloadDialogFragment())
+            launch {
+                initialSettingViewModel.isInitialSetting.collect {
+                    if (it != null) {
+                        if (it) findNavController().safeNavigate(IntroFragmentDirections.actionIntroFragmentToSignInFragment())
+                        else findNavController().safeNavigate(IntroFragmentDirections.actionIntroFragmentToInitialDialogFragment())
+                        hideLoading()
+                    }
                 }
             }
-        })
+        }
     }
 }
--- app/src/main/java/io/orkk/vietnam/screen/intro/IntroViewModel.kt
+++ app/src/main/java/io/orkk/vietnam/screen/intro/IntroViewModel.kt
@@ -1,23 +1,8 @@
 package io.orkk.vietnam.screen.intro
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import dagger.hilt.android.lifecycle.HiltViewModel
-import io.orkk.vietnam.data.remote.file.FileRepository
 import io.orkk.vietnam.screen.BaseViewModel
-import io.orkk.vietnam.utils.event.Event
 import javax.inject.Inject
 
 @HiltViewModel
-class IntroViewModel @Inject constructor(
-    private val fileRepository: FileRepository
-) : BaseViewModel() {
-
-    private val _navigateToSignIn = MutableLiveData<Event<Unit>>()
-    val navigateToSignIn: LiveData<Event<Unit>>
-        get() = _navigateToSignIn
-
-    fun navigateToMain() {
-        _navigateToSignIn.value = Event(Unit)
-    }
-}
+class IntroViewModel @Inject constructor() : BaseViewModel()
